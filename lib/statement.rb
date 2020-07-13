e1611c07569bfd39f56543d47c7c7df89bfbda74
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-13 18:49:29 +0100

    Still working on getting formatting right

--- lib/statement.rb
+++ lib/statement.rb
@@ -4,4 +4,12 @@ class Statement
   def header
     'date || credit || debit || balance'
   end
+
+  def print(transactions)
+    arr = transactions.each_with_index do |value, index|
+      value[index].join(' || ')
+      index += 1
+    end
+    arr.join('\n')
+  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -11,4 +11,8 @@ describe Statement do
   it 'should return a header string for the statement' do
     expect(subject.header).to eq 'date || credit || debit || balance'
   end
+
+  it 'should print each element from transactions array' do
+    expect(subject.print(transactions)).to eq '13/07/2020 || 50 ||  || 100\n14/07/2020 ||  || 50 || 50'
+  end
 end
