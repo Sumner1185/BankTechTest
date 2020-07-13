ab5730e8e422b6cce169811e7d97cd90e50fca4b
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-13 16:45:50 +0100

    Refactor activity test file to remove repetition

--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -3,19 +3,18 @@
 require 'activity'
 
 describe Activity do
+  subject(:list) { Activity.new }
+
   it 'should instantiate with an empty transactions array' do
-    list = Activity.new
     expect(list.transactions.length).to eq 0
   end
 
   it 'should add to transactions when a deposit is made' do
-    list = Activity.new
     list.add_deposit(50)
     expect(list.transactions.length).to eq 1
   end
 
   it 'should add to transactions when a withdrawal is made' do
-    list = Activity.new
     list.add_withdrawal(50)
     expect(list.transactions.length).to eq 1
   end
