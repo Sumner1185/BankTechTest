03b2c939b30e7609c9dd05c728c190830ac0ed1b
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-13 11:47:36 +0100

    Refactor test file to use same variable for new account

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,14 +1,13 @@
 require 'bank_account'
 
 describe BankAccount do
+  subject(:account) { BankAccount.new }
   
   it 'should initialise with a balance of 0' do
-    account = BankAccount.new
     expect(account.balance).to eq 0
   end
 
   it 'should credit account when deposit is made' do
-    account = BankAccount.new
     account.deposit(50)
     expect(account.balance).to eq 50
   end
