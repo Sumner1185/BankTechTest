e39bcfcdc4aab0a919bc96d237f64ae60d37bf85
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-14 17:35:28 +0100

    Add tests for empty values for credit or withdraw

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,11 +2,12 @@
 
 # class in charge of handling each transaction
 class Transaction
-  attr_reader :date, :credit, :withdraw
+  attr_reader :date, :credit, :withdraw, :balance
 
-  def initialize(date, credit, withdraw)
+  def initialize(date, credit, withdraw, balance)
     @date = date
     @credit = credit
     @withdraw = withdraw
+    @balance = balance
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,25 +3,37 @@
 require 'transaction'
 
 describe Transaction do
-  before(:each) do
-    @transaction = described_class.new('07/13/2012', '100.00', '50.00')
-  end
-  
-  it 'should store a date of the transaction' do
-    expect(@transaction.date).to eq '07/13/2012'
+  describe 'non-empty values passed' do
+    before(:each) do
+      @transaction = described_class.new('07/13/2012', '100.00', '50.00', '500.00')
+    end
+
+    it 'should store a date of the transaction' do
+      expect(@transaction.date).to eq '07/13/2012'
+    end
+
+    it 'should store an amount of credit' do
+      expect(@transaction.credit).to eq '100.00'
+    end
+
+    it 'should store an amount of withdraw' do
+      expect(@transaction.withdraw).to eq '50.00'
+    end
+
+    it 'should store a balance' do
+      expect(@transaction.balance).to eq '500.00'
+    end
   end
 
-  it 'should store an amount of credit' do
-    expect(@transaction.credit).to eq '100.00'
-  end
+  describe 'empty values passed' do
+    it 'should take an empty string if no credit given' do
+      transaction = Transaction.new('date', '', '50.00', '500.00')
+      expect(transaction.credit).to eq ''
+    end
 
-  # it 'should have a default value of '' if no credit given' do
-  #   transaction = Transaction.new('date', '', '50.00')
-  #   expect(transaction.credit).to eq ''
-  # end
-
-  it 'should store an amount of withdraw' do
-    expect(@transaction.withdraw).to eq '50.00'
+    it 'should take an empty string if no withdraw given' do
+      transaction = Transaction.new('date', '50.00', '', '500.00')
+      expect(transaction.withdraw).to eq ''
+    end
   end
-  
 end
