f0ce960fa688a4e9b8da4044ab4f825aaf352e4f
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-15 14:08:39 +0100

    successful mocking of Transactions for statement test

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -9,9 +9,13 @@ describe Statement do
   end
 
   it 'should print each element from transactions array' do
-    transactions = 
-    [@date="13/07/2020", @credit="50.00", @debit="", @balance="100.00"],
-    [@date="14/07/2020", @credit="", @debit="50.00", @balance="50.00"]
+    transactions = []
+    transaction_1 = instance_double('Transaction')
+    transaction_2 = instance_double('Transaction')
+    allow(transaction_1).to receive_messages(date: "13/07/2020", credit: "50.00", debit: "", balance: "100.00")
+    allow(transaction_2).to receive_messages(date: "14/07/2020", credit: "", debit: "50.00", balance: "50.00")
+    transactions.push(transaction_1, transaction_2)
+
     expect do
       subject.print(transactions)
     end.to output("date || credit || debit || balance\n14/07/2020 ||  || 50.00 || 50.00\n13/07/2020 || 50.00 ||  || 100.00\n").to_stdout
